a35a6e1ce1efe52ce0fc2d6da47556dc780c3314
brandondove <brandon@pixeljar.net>
2011-03-26 05:40:19 -0700

    Simplified option names. It was getting too hard to remember what they were.

--- install.php
+++ install.php
@@ -7,12 +7,12 @@
 */
 
 $defaults = array(
-	'favicon'						=> '<empty>',
-	'create-from-jpg-support'		=> ( function_exists( 'imagecreatefromjpeg' ) ? true : false ),
-	'create-from-gif-support'		=> ( function_exists( 'imagecreatefromgif' ) ? true : false ),
-	'create-from-png-support'		=> ( function_exists( 'imagecreatefrompng' ) ? true : false ),
-	'png-alpha-blending-support'	=> ( function_exists( 'imagealphablending' ) ? true : false ),
-	'png-save-alpha-support'		=> ( function_exists( 'imagesavealpha' ) ? true : false ),
+	'favicon'				=> '<empty>',
+	'jpg'					=> ( function_exists( 'imagecreatefromjpeg' ) ? true : false ),
+	'gif'					=> ( function_exists( 'imagecreatefromgif' ) ? true : false ),
+	'png'					=> ( function_exists( 'imagecreatefrompng' ) ? true : false ),
+	'png-alpha-blending'	=> ( function_exists( 'imagealphablending' ) ? true : false ),
+	'png-save-alpha'		=> ( function_exists( 'imagesavealpha' ) ? true : false ),
 );
 
 $saved = get_option( PJFAV_ADMIN_OPTIONS );
