5c72af690f8d71f8c1f0ebefa2150153f422fe28
brandondove <brandon@pixeljar.net>
2011-03-26 02:23:45 -0700

    Changed stored values from yes/no to true/false

--- install.php
+++ install.php
@@ -8,11 +8,11 @@
 
 $defaults = array(
 	'favicon'						=> '<empty>',
-	'create-from-jpg-support'		=> ( function_exists( 'imagecreatefromjpeg' ) ? 'yes' : 'no' ),
-	'create-from-gif-support'		=> ( function_exists( 'imagecreatefromgif' ) ? 'yes' : 'no' ),
-	'create-from-png-support'		=> ( function_exists( 'imagecreatefrompng' ) ? 'yes' : 'no' ),
-	'png-alpha-blending-support'	=> ( function_exists( 'imagealphablending' ) ? 'yes' : 'no' ),
-	'png-save-alpha-support'		=> ( function_exists( 'imagesavealpha' ) ? 'yes' : 'no' ),
+	'create-from-jpg-support'		=> ( function_exists( 'imagecreatefromjpeg' ) ? true : false ),
+	'create-from-gif-support'		=> ( function_exists( 'imagecreatefromgif' ) ? true : false ),
+	'create-from-png-support'		=> ( function_exists( 'imagecreatefrompng' ) ? true : false ),
+	'png-alpha-blending-support'	=> ( function_exists( 'imagealphablending' ) ? true : false ),
+	'png-save-alpha-support'		=> ( function_exists( 'imagesavealpha' ) ? true : false ),
 );
 
 $saved = get_option( PJFAV_ADMIN_OPTIONS );
