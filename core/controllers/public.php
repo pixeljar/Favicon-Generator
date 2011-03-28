cd1b3c4d1a11d267ceb9e1c66ec6a0a74e72a449
brandondove <brandon@pixeljar.net>
2011-03-28 00:43:37 -0700

    Instantiated the class

--- core/controllers/public.php
+++ core/controllers/public.php
@@ -3,14 +3,15 @@
 class pjfav_public_controller {
 	
 	function __construct() {
-		add_action( 'wp_head', array( &$this, 'wp_head' ) );
+		add_action( 'wp_head', array( &$this, 'wp_head' ), 1 );
 	}
 	
 	function wp_head() {
-		//this is a sample function that includes additional styles within the head of your template.
+		// Add the shortcut icon to the head
 		echo '<link rel="shortcut icon" href="'.PJFAV_URL.'favicon.ico" />';
 		// echo '<link rel="apple-touch-icon" href="'.PJFAV_URL.'touchicon.png" />'; // coming soon!
 		
 		echo '<meta name="generator" content="Think-Press Favicon Generator v1.5" />';
 	}
 }
+$pjfav_public_controller = new pjfav_public_controller;
